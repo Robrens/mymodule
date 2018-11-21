1231c8c12fffbd749e061a9e6f9d3d3778ce0e41
Robrens <jbb.baudu@wanadoo.fr>
2018-11-21 14:34:33 +0100

    module template

--- mymodule.php
+++ mymodule.php
@@ -134,4 +134,27 @@ class MyModule extends Module
         return $helper->generateForm($fields_form);
     }
 
+    public function hookDisplayLeftColumn($params)
+    {
+        $this->context->smarty->assign(
+            array(
+                'my_module_name' => Configuration::get('MYMODULE_NAME'),
+                'my_module_link' => $this->context->link->getModuleLink('mymodule', 'display'),
+                'my_module_message' => $this->l('This is a simple text message') // Do not forget to enclose your strings in the l() translation method
+            )
+        );
+    
+        return $this->display(__FILE__, 'mymodule.tpl');
+    }
+    
+    public function hookDisplayRightColumn($params)
+    {
+        return $this->hookDisplayLeftColumn($params);
+    }
+    
+    public function hookDisplayHeader()
+    {
+        $this->context->controller->addCSS($this->_path.'css/mymodule.css', 'all');
+    }
+
 }
---
+++ views/templates/front/display.tpl
@@ -0,0 +1,4 @@
+{extends file="page.tpl"}
+{block name='page_content'}
+<h1>“Welcome to my shop!”</h1>
+{/block}
